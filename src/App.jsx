3b0d209ad6afacaa672e7c6bb52c9f78dc185131
joehobos <joehobos@DESKTOP-A520OBQ>
2023-06-15 15:57:42 -0700

    fixed onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { puppyList } from "./data.js";
+import './App.css'
+import './index.css'
 
 
 function App() {
@@ -7,9 +9,8 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null)
 
-  function handleClick(puppy) {
-    setFeatPupId(puppy.id)
-    
+  function handleClick(id) {
+    setFeatPupId(id)
   }
   const featuredPup = puppies.find((pup)=> pup.id === featPupId
   )
@@ -26,18 +27,18 @@ function App() {
               <li>Age: {featuredPup.age}</li>
               <li>Email: {featuredPup.email}</li>
             </ul>
-            </div>
+          </div>
         )
       }
       {
         puppies.map((puppy) => {
              return (
-             <p onClick={handleClick} key={puppy.id}>{puppy.name}</p>
+              <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+              {puppy.name}
+            </p>
              );
            })
-        
        }
-      
     </div>
   );
 }
