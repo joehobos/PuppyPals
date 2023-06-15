a642899e23588b0e2a1f3e35466585fe870a404b
joehobos <joehobos@DESKTOP-A520OBQ>
2023-06-15 15:36:11 -0700

    add on click/render

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,39 @@ import { puppyList } from "./data.js";
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
 
+  function handleClick(puppy) {
+    setFeatPupId(puppy.id)
+    
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId
+  )
+  console.log(featuredPup)
   console.log("puppyList: ", puppyList);
 
   return (
     <div className="App">
       {
+        featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+            </div>
+        )
+      }
+      {
         puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
+             return (
+             <p onClick={handleClick} key={puppy.id}>{puppy.name}</p>
+             );
            })
+        
        }
+      
     </div>
   );
 }
